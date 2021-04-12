2c31d80325c15d5f19aa0746744d13afc916b94a
Tharusha Wijayabahu <Tharusha>
2021-04-12 10:56:42 +0530

    Saving file

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ def upload_image():
         if request.files:
             image = request.files["image"]
 
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image))
+
             print(image)
 
             return redirect(request.url)
